4759fb429924ab27fb7a33363dadb57f23d9b372
Claudia Font <claudiafontmarques@gmail.com>
2021-11-26 17:18:46 +0100

    added configuration eslint-husky-1rstest

---
+++ App.test.tsx
@@ -0,0 +1,10 @@
+import App from "./App";
+import React from "react";
+import { render } from "@testing-library/react-native";
+
+test("hola", () => {
+  const screen = render(<App />);
+  /*   const { getByText } = render(<App />);
+   */
+  expect(screen.getByText("Hola classe!")).not.toBe(null);
+});
