99fd166cc1c520b2286eb0d551f8a9390f8fd938
Claudia Font <claudiafontmarques@gmail.com>
2021-11-26 18:47:14 +0100

    probe to install yarn again

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola classe!qw</Text>
+      <Text>Hola classe!</Text>
       <StatusBar style="auto" />
     </View>
   );
