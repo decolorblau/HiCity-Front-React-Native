a2806d29303b486f515e8a023c6235b7797cdde7
Claudia Font <claudiafontmarques@gmail.com>
2021-11-29 20:54:20 +0100

    deleted app.test

--- App.test.tsx
+++ App.test.tsx
@@ -1,8 +1,1 @@
-import App from "./App";
-import React from "react";
-import { render } from "@testing-library/react-native";
 
-test("hola", () => {
-  const screen = render(<App />);
-  expect(screen.getByText("Hola Mario!")).not.toBe(null);
-});
