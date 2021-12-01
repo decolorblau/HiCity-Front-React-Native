64ceb971f820803e530d7f09c4293a9614ac5a5b
Claudia Font <claudiafontmarques@gmail.com>
2021-12-01 09:54:29 +0100

    added function to open map in user position

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -11,11 +11,12 @@ const Map = () => {
   const [error, setError] = useState("");
   const [message, setMessage] = useState("");
   const [mapRegion, setMapRegion] = useState({
-    latitude: 41.38879,
+    latitude: 1.38879,
     longitude: 2.15899,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   });
+  const mapRef = React.useRef();
 
   useEffect(() => {
     (async () => {
@@ -28,7 +29,13 @@ const Map = () => {
       const newLocation = await Location.getCurrentPositionAsync({
         accuracy: Location.Accuracy.Balanced,
       });
-
+      console.log(newLocation.coords.latitude, newLocation.coords.longitude);
+      const region = {
+        latitude: newLocation.coords.latitude,
+        longitude: newLocation.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      };
       setMapRegion({
         latitude: newLocation.coords.latitude,
         longitude: newLocation.coords.longitude,
@@ -37,29 +44,29 @@ const Map = () => {
       });
 
       setLocation(newLocation);
+      goToMyLocation(region);
     })();
 
     setMessage("Waiting...");
     if (error !== "") {
       setMessage(error);
     } else setMessage(JSON.stringify(location));
-  }, [location]);
+  }, []);
 
+  const goToMyLocation = async (region: any) => {
+    mapRef.current.animateCamera({
+      center: region,
+    });
+  };
+
+  console.log(mapRegion);
   return (
     <View style={styles.container}>
       <Text>{message}</Text>
       <MapView
         style={styles.map}
-        initialRegion={
-          mapRegion
-
-          /* {
-          longitude: 41.38879,
-          latitude: 2.15899,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        } */
-        }
+        ref={mapRef}
+        initialRegion={mapRegion}
         provider={PROVIDER_GOOGLE}
         customMapStyle={mapStyle}
         showsUserLocation={true}
