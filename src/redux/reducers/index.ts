5e0de9fb23147d9fdf71b97897111e34c70cbe41
Claudia Font <claudiafontmarques@gmail.com>
2021-12-08 12:39:09 +0100

    deleted current reducer unused

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
-import currentLandmarksReducer from "./currentLandmarksReducer";
 import landmarksReducer from "./landmarksReducer";
 import { userReducer } from "./userReducer";
 
 export const rootReducer = combineReducers({
   landmarks: landmarksReducer,
-  landmark: currentLandmarksReducer,
   user: userReducer,
 });
 export type RootState = ReturnType<typeof rootReducer>;
